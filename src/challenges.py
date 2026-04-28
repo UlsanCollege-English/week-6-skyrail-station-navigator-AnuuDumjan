a5b5c7ce1d7d4d6243ca223613b80be515e63d1b
Anu Dumjan <anudumjan2@gmail.com>
2026-04-28 21:01:16 +0545

    Completed week 6 project

--- src/challenges.py
+++ src/challenges.py
@@ -19,40 +19,35 @@ class TreeNode:
         self.right = right
 
 
-
 def preorder_values(root: TreeNode | None) -> list[Any]:
     """Return the tree values in preorder: node, left, right."""
-    def preorder_values(root: TreeNode | None) -> list[Any]:
-     if root is None:
+    if root is None:
         return []
-    return [root.value] + preorder_values(root.left) + preorder_values(root.right)
 
+    return [root.value] + preorder_values(root.left) + preorder_values(root.right)
 
 
 def inorder_values(root: TreeNode | None) -> list[Any]:
     """Return the tree values in inorder: left, node, right."""
-    def inorder_values(root: TreeNode | None) -> list[Any]:
-     if root is None:
+    if root is None:
         return []
-    return inorder_values(root.left) + [root.value] + inorder_values(root.right)
 
+    return inorder_values(root.left) + [root.value] + inorder_values(root.right)
 
 
 def postorder_values(root: TreeNode | None) -> list[Any]:
     """Return the tree values in postorder: left, right, node."""
-    def postorder_values(root: TreeNode | None) -> list[Any]:
-     if root is None:
+    if root is None:
         return []
-    return postorder_values(root.left) + postorder_values(root.right) + [root.value]
 
+    return postorder_values(root.left) + postorder_values(root.right) + [root.value]
 
 
 def bst_contains(root: TreeNode | None, target: int) -> bool:
     """Return True if target exists in the BST. Otherwise return False."""
-    def bst_contains(root: TreeNode | None, target: int) -> bool:
-     if root is None:
+    if root is None:
         return False
-    
+
     if target == root.value:
         return True
     elif target < root.value:
@@ -61,19 +56,17 @@ def bst_contains(root: TreeNode | None, target: int) -> bool:
         return bst_contains(root.right, target)
 
 
-
 def bst_insert(root: TreeNode | None, value: int) -> TreeNode:
     """Insert value into the BST and return the root node.
 
     Duplicate values should be ignored.
     """
-    def bst_insert(root: TreeNode | None, value: int) -> TreeNode:
-     if root is None:
+    if root is None:
         return TreeNode(value)
-    
+
     if value < root.value:
         root.left = bst_insert(root.left, value)
     elif value > root.value:
         root.right = bst_insert(root.right, value)
-    
+
     return root
